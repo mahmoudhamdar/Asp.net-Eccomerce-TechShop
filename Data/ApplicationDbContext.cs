47c7ec69ad815c9b8b36a2407d87a54e00b500c6
mahmoud <mahmoud.hamdar2001@hotmail.com>
2024-05-26 17:14:29 +0300

    Finished

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -29,29 +29,28 @@ namespace WebApplication1.Data
             base.OnModelCreating(modelBuilder);
             modelBuilder.Entity<Product>().HasData(
                 new Product
-                { ProductID = 1, Name = "Razer Headphones", Description = "", Price = 30, ImgUrl = "", CategoryId = 3 },
+                { ProductID = 1, Name = "Legion Mouse x1", Description = "", Price = 30, ImgUrl = @"\images\product\download.jpg", CategoryId = 2 },
                 new Product
-                { ProductID = 2, Name = "HyperX Mouse", Description = "", Price = 110, ImgUrl = "", CategoryId = 2 },
+                { ProductID = 2, Name = "Legion Mouse x2", Description = "", Price = 110, ImgUrl = @"\images\product\download (1).jpg", CategoryId = 2 },
                  new Product
-                 { ProductID = 3, Name = "Razor Keyboard", Description = "", Price = 70, ImgUrl = "", CategoryId = 1 },
+                 { ProductID = 3, Name = "Razor Keyboard K4", Description = "", Price = 70, ImgUrl = @"\images\product\https___hybrismediaprod.blob.core.windows.net_sys-master-phoenix-images-container_h61_h66_9705254158366_huntsman-v3-pro-mini-2-500x500.png", CategoryId = 1 },
                  new Product
-                     { ProductID = 4, Name = "Razor Headphones2", Description = "", Price = 30, ImgUrl = "", CategoryId = 3 },
+                     { ProductID = 4, Name = "Razor Headphones Kraken", Description = "", Price = 30, ImgUrl = @"\images\product\krakens.jpg", CategoryId = 3 },
                  new Product
-                     { ProductID = 5, Name = "HyperX Mouse2", Description = "", Price = 110, ImgUrl = "", CategoryId = 2 },
+                     { ProductID = 5, Name = "Razor Keyboard K3", Description = "", Price = 110, ImgUrl = @"\images\product\https___hybrismediaprod.blob.core.windows.net_sys-master-phoenix-images-container_h8f_h57_9640099217438_blackwidow-v4-black-x-500x500.png", CategoryId = 1 },
                  new Product
-                     { ProductID = 6, Name = "Razor Keyboard2", Description = "", Price = 70, ImgUrl = "", CategoryId = 1 },
+                     { ProductID = 6, Name = "Razor Keyboard K5", Description = "", Price = 70, ImgUrl = @"\images\product\https___hybrismediaprod.blob.core.windows.net_sys-master-phoenix-images-container_hf7_h5a_9640099119134_blackwidow-v4-75-black-2-500x500.png", CategoryId = 1 },
                  new Product
-                     { ProductID = 7, Name = "Razor Headphones2", Description = "", Price = 30, ImgUrl = "", CategoryId = 3 },
+                     { ProductID = 7, Name = "Razor cloudII", Description = "", Price = 30, ImgUrl = @"\images\product\headphones.jpg", CategoryId = 3 },
                  new Product
-                     { ProductID = 8, Name = "HyperX Mouse2", Description = "", Price = 110, ImgUrl = "", CategoryId = 2 },
-                 new Product
-                     { ProductID = 9, Name = "Razor Keyboard2", Description = "", Price = 70, ImgUrl = "", CategoryId = 1 }
+                     { ProductID = 8, Name = "HyperX Mouse PulseFire", Description = "", Price = 110, ImgUrl = @"\images\product\Hyper_mouse.jpg", CategoryId = 2 }
+                 
             );
             modelBuilder.Entity<Category>().HasData(
               new Category
               { CategoryId = 1, Name = "Keyboards", DisplayOrder = 1, CreatedDateTime = DateTime.Now.Date },
               new Category
-              { CategoryId = 2, Name = "Mouses", DisplayOrder = 2, CreatedDateTime = DateTime.Now.Date },
+              { CategoryId = 2, Name = "Mices", DisplayOrder = 2, CreatedDateTime = DateTime.Now.Date },
                new Category
                { CategoryId = 3, Name = "Headphones", DisplayOrder = 3, CreatedDateTime = DateTime.Now.Date }
           );
@@ -63,13 +62,6 @@ namespace WebApplication1.Data
                 new Company
                     { CompanyId = 3, Name = "Legion", PhoneNumber = "02123599",City = "Saida",StreetAddress = "" }
             );
-            modelBuilder.Entity<User>().HasData(
-                new User
-                    { UserName ="Admin" , PhoneNumber = "01334455",Email = "Beirut",Address = "Beirut"},
-                new User
-                    { UserName = "Customer",  PhoneNumber = "05557878",Email = "Tyre",Address = "Tyre" }
-               
-            ); 
            
             
         }
